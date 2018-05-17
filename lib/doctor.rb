8cba3914d6d1628c2d97c0e7f37486d0d8d6e4b1
Eric Fiorani <efiorani305@gmail.com>
2018-05-17 19:20:46 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,19 +1,12 @@
 class Doctor
-  #has_many appointments, has_many patients through appointments
-  attr_accessor :name, :appointments
 
+  attr_accessor :name
+
+  @@all = []
 
   def initialize(name)
     @name = name
-    @appointments = []
-  end
-
-  def add_appointment(appointment)
-    @appointments << appointment
-  end
-
-  def patients
-    self.appointments.collect{|a| a.patient}
+    @@all << self
   end
 
 end
