76f1b7e97c2b786bb54964ac5f978cff42e0f108
Eric Fiorani <efiorani305@gmail.com>
2018-05-17 19:13:58 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -14,8 +14,8 @@ class Artist
   end
 
   def new_song(name, genre)
-    Song.new(name, self, genre)
-    song.artist
+    song = Song.new(name, self, genre)
+    song.artist = self
   end
 
   def songs
